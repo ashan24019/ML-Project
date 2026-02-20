2b1f59f1d3b6e5a1215ef041eb6170c2b0095da9
Ashan <ashanvimodh241@gmail.com>
2026-02-20 23:36:08 +0530

    data transformation pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ from src.exception import CustomException
 import sys
 
 
+from src.components.data_tranformation import DataTransformation, DataTransformationConfig
+
+
 @dataclass
 class DataIngestionConfig:
     raw_data_path: str=os.path.join("artifacts", "data.csv")
@@ -50,6 +53,7 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj= DataIngestion()
-    train_path, test_path = obj.initiate_data_ingestion()
-    print("Train data saved at:" , train_path)
-    print("Test data saved at:" , test_path)
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation()
